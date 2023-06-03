7ed72ff94d2b0cedfa783a1984f0ebd72c15e3ff
TusharRB <91899841+TusharRB@users.noreply.github.com>
2023-06-03 22:59:04 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,26 @@
 //your JS code here. If required.
+const animalList = [
+  "camel",
+  "cheetah",
+  "elephant",
+  "gorilla",
+  "koala",
+  "lion",
+  "polar_bear",
+  "tiger",
+  "wolf"
+];
+
+const photoContainer = document.getElementById("photos");
+
+animalList.forEach(animal => {
+  const img = document.createElement("img");
+  img.src = `https://www2.d125.org/applied_arts/teched/courses/WEB/portfolios/2014_FallP8/Rami_Portfolio_Period8/unit4_css/project_1folder/cssProject1/${animal}.jpg`;
+  img.alt = animal;
+  
+  const photoDiv = document.createElement("div");
+  photoDiv.classList.add("photo");
+  photoDiv.appendChild(img);
+  
+  photoContainer.appendChild(photoDiv);
+});
